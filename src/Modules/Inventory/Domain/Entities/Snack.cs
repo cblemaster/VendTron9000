525b3a28e98fa2f3ffc756b63136769dfd2ffe6b
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-06 15:57:20 -0400

    snack entity

--- src/Modules/Inventory/Domain/Entities/Snack.cs
+++ src/Modules/Inventory/Domain/Entities/Snack.cs
@@ -1,7 +1,26 @@
 ﻿
 namespace Modules.Inventory.Domain.Entities;
 
-internal class Snack
+internal class Snack : Entity<Snack>
 {
+    internal string Label { get; init; }
+    internal decimal Price { get; init; }
+    internal decimal Cost { get; init; }
+    internal SnackType SnackType { get; init; }
+    internal string MachineInventoryIndex { get; init; }
+    internal DateTimeOffset DateAddedToMachineInventory { get; init; }
+    internal override Identifier<Snack> Id { get; init; }
 
+    internal Snack(string label, decimal price, decimal cost, string snackType, string index, DateTimeOffset dateAdded, Guid id)
+    {
+        Label = label;
+        Price = price;
+        Cost = cost;
+        SnackType = new SnackType(snackType);
+        MachineInventoryIndex = index;
+        DateAddedToMachineInventory = dateAdded;
+        Id = new Identifier<Snack>(id);
+    }
+
+    internal Snack(string label, decimal price, decimal cost, string snackType, string index) : this(label, price, cost, snackType, index, DateTimeOffset.UtcNow, Guid.NewGuid()) { }
 }
