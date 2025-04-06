1fd2851b032f33b9f7582af0c623a469224c280e
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-06 15:04:35 -0400

    snack type entity

--- src/Modules/Inventory/Entities/SnackType.cs
+++ src/Modules/Inventory/Entities/SnackType.cs
@@ -1,7 +1,16 @@
 ﻿
 namespace Modules.Inventory.Domain.Entities;
 
-internal class SnackType
+internal class SnackType : Entity<SnackType>
 {
+    internal string Descriptor { get; init; } = string.Empty;
+    internal override Identifier<SnackType> Id { get; init; }
 
+    internal SnackType(string descriptor, Guid id)
+    {
+        Descriptor = descriptor;
+        Id = new Identifier<SnackType>(id);
+    }
+
+    internal SnackType(string descriptor) : this(descriptor, Guid.NewGuid()) { }
 }
