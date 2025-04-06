13818f70c8dd658158e98695a4009c3090bd2e07
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-06 19:34:13 -0400

    add dtos

---
+++ src/Modules/Inventory/Persistence/InventoryDTO.cs
@@ -0,0 +1,4 @@
+﻿
+namespace Modules.Inventory.Persistence;
+
+internal record InventoryDTO(IEnumerable<SnackDTO> Snacks, int InventoryTypeId, Guid Id);
---
+++ src/Modules/Inventory/Persistence/SnackDTO.cs
@@ -0,0 +1,4 @@
+﻿
+namespace Modules.Inventory.Persistence;
+
+internal record SnackDTO(string Label, decimal Price, decimal Cost, int SnackTypeId, string MachineInventoryIndex, int InventoryId, DateTimeOffset DateAddedToMachineInventory, Guid Id);
