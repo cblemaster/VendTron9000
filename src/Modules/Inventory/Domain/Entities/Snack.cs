5600916b7155a42d118b992eea6311383a2f074b
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-06 17:37:53 -0400

    add validation

--- src/Modules/Inventory/Domain/Entities/Snack.cs
+++ src/Modules/Inventory/Domain/Entities/Snack.cs
@@ -16,6 +16,8 @@ internal class Snack : Entity<Snack>
 
     internal Snack(string label, decimal price, decimal cost, SnackType snackType, string index, Inventory inventory, DateTimeOffset dateAdded, Guid id)
     {
+        ValidateParams(label, price, cost, snackType, index, inventory);
+
         Label = label;
         Price = price;
         Cost = cost;
@@ -24,6 +26,34 @@ internal class Snack : Entity<Snack>
         Inventory = inventory;
         DateAddedToMachineInventory = dateAdded;
         Id = new Identifier<Snack>(id);
+
+        static void ValidateParams(string label, decimal price, decimal cost, SnackType snackType, string index, Inventory inventory)
+        {
+            if (string.IsNullOrWhiteSpace(label) || label.Length > 12)
+            {
+                throw new ArgumentException("Label is required and must be fewer than 12 characters in length...", nameof(label));
+            }
+            if (price <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(price), "Price must be greater than zero...");
+            }
+            if (cost <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(cost), "Cost must be greater than zero...");
+            }
+            if (snackType == SnackType.NotSet)
+            {
+                throw new ArgumentException($"{snackType.ToString()} is invalid...", nameof(snackType));
+            }
+            if (string.IsNullOrWhiteSpace(index))
+            {
+                throw new ArgumentException("Index is invalid...", nameof(index));
+            }
+            if (inventory is null)
+            {
+                throw new ArgumentNullException(nameof(inventory), "Inventory is required...");
+            }
+        }
     }
 
     internal Snack(string label, decimal price, decimal cost, SnackType snackType, string index, Inventory inventory) : this(label, price, cost, snackType, index, inventory, DateTimeOffset.UtcNow, Guid.NewGuid()) { }
