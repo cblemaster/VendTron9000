73a5e7ff3546cdc4461374235bed56ab6d105349
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-06 15:03:48 -0400

    remove ctors from abstract class

--- src/Modules/Inventory/Entities/Entity.cs
+++ src/Modules/Inventory/Entities/Entity.cs
@@ -4,8 +4,4 @@ namespace Modules.Inventory.Domain.Entities;
 internal abstract class Entity<T>
 {
     internal abstract Identifier<T> Id { get; init; }
-
-    internal Entity(Guid id) => Id = new Identifier<T>(id);
-
-    internal Entity() => Id = new Identifier<T>();
 }
