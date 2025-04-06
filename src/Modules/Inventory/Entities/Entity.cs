b9f6c66ba226df472a08caa29d06e9ca69bb8139
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-06 14:53:05 -0400

    entity abstract class

--- src/Modules/Inventory/Entities/Entity.cs
+++ src/Modules/Inventory/Entities/Entity.cs
@@ -1,7 +1,11 @@
 ﻿
 namespace Modules.Inventory.Domain.Entities;
 
-internal class Entity
+internal abstract class Entity<T>
 {
+    internal abstract Identifier<T> Id { get; init; }
 
+    internal Entity(Guid id) => Id = new Identifier<T>(id);
+
+    internal Entity() => Id = new Identifier<T>();
 }
