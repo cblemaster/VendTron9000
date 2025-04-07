9601c59567d8afc13cd325eecc3310c5a8c30790
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-06 20:49:30 -0400

    add db context

---
+++ src/Modules/Inventory/Persistence/InventoryDbContext.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Modules.Inventory.Persistence;
+
+internal sealed class InventoryDbContext : DbContext
+{
+    private const string CONNSTRING = "Server=.;Database=VendTron9000;Trusted_Connection=true;Trust Server Certificate=true";
+
+    internal InventoryDbContext() { }
+
+    public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(CONNSTRING);
+    override protected void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // specify schema
+
+        // inventory entity
+        // to table
+        // key
+        // props
+        
+        // snack entity
+        // to table
+        // key
+        // props
+        // fk : snack has one inventory with many snacks; join snack.inventoryId to inventory.id
+    }
+}
