916a41a2f4a8150d8cbf9da3f282c3e79c160172
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-07 18:28:45 -0400

    add descriptor value object

---
+++ src/Modules/Inventory/Domain/ValueObjects/Descriptor.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Modules.Inventory.Domain.ValueObjects;
+
+internal sealed class Descriptor
+{
+    internal string Value { get; init; }
+
+    internal Descriptor(string value, uint maxLength, bool isRequired, bool isAllowAllWhitespace, string? regexPattern)
+    {
+        if (isRequired && string.IsNullOrEmpty(value))
+        {
+            throw new ArgumentException("Value is required...", nameof(value));
+        }
+        
+        if (!isAllowAllWhitespace && Regex.Match(value, @"\s$").Success)
+        {
+            throw new ArgumentException("Value must not be all whitespace characters...", nameof(value));
+        }
+
+        if (value.Length > maxLength)
+        {
+            throw new ArgumentException($"Value must be fewer than {maxLength} characters in length...", nameof(value));
+        }
+
+        if (regexPattern is not null && !Regex.Match(value, regexPattern).Success)
+        {
+            throw new ArgumentException($"Value must be formatted properly...", nameof(value));
+        }
+
+        Value = value;
+    }
+}
