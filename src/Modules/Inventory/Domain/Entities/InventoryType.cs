a04df0b200dcc00cfd82d1a1f78cdbd98ce04d4c
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-09 16:41:15 -0400

    add inventory type entity

---
+++ src/Modules/Inventory/Domain/Entities/InventoryType.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Modules.Inventory.Domain.Entities;
+
+internal sealed class InventoryType : Entity<InventoryType>
+{
+    internal string Name { get; init; }
+    public override Identifier<InventoryType> Id { get; init; }
+
+    public InventoryType(string name, Guid id)
+    {
+        Name = name;
+        Id = new Identifier<InventoryType>(id);
+    }
+
+    public InventoryType(string name)
+    {
+        Name = name;
+        Id = new Identifier<InventoryType>();
+    }
+}
