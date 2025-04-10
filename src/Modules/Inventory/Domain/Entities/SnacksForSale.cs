f020ab36d410554dc34101b5aba2d7c5b0cfb598
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-09 21:22:14 -0400

    update snacks for sale

--- src/Modules/Inventory/Domain/Entities/SnacksForSale.cs
+++ src/Modules/Inventory/Domain/Entities/SnacksForSale.cs
@@ -1,27 +1,36 @@
-﻿namespace Modules.Inventory.Domain.Entities;
+﻿
+using System.Text.Json;
 
-internal class SnacksForSale : Entity<SnacksForSale>
+namespace Modules.Inventory.Domain.Entities;
+
+internal class SnacksForSale
 {
-    internal IEnumerable<Snack> Snacks { get; init; } = [];
-    internal override Identifier<SnacksForSale> Id { get; init; }
+    private const int MAX_COUNT_OF_SNACK = 5;
 
-    private SnacksForSale() { }
+    internal IEnumerable<SnackLocation> SnackLocations { get; init; } = [];
 
-    internal SnacksForSale(IEnumerable<Snack> snacks, Guid id)
-    {
-        Snacks = snacks;
-        Id = new Identifier<SnacksForSale>(id);
-    }
+    //internal SnacksForSale() => Id = new Identifier<SnacksForSale>();
 
-    internal SnacksForSale(IEnumerable<Snack> snacks) : this(snacks, Guid.NewGuid()) { }
+    //private static List<Snack> SnackInventory() =>
+    //    [
+    //        new("Potato Crisps", 3.05m, 2.70m, "Chips", "A1"),
+    //        new("Prongles", 1.45m, 1.25m, "Chips", "A2"),
+    //        new("Boogles", 2.75m, 2.40m, "Chips", "A3"),
+    //        new("Veggie Crisps", 3.65m, 2.95m, "Chips", "A4"),
 
-    internal void AddSnacks(IEnumerable<Snack> snacks) => Snacks.ToList().AddRange(snacks);
+    //        new("Lieutenants Wafers", 1.80m, 1.50m, "Crackers", "B1"),
+    //        new("Chweesits", 1.50m, 1.05m, "Crackers", "B2"),
+    //        new("Pita Squares", 1.50m, 1.25m, "Crackers", "B3"),
+    //        new("Nyblets", 1.75m, 1.35m, "Crackers", "B4"),
 
-    internal void RemoveSnack(Snack snack)
-    {
-        if (Snacks.Contains(snack))
-        {
-            _ = Snacks.ToList().Remove(snack);
-        }
-    }
+    //        new("Cola", 1.25m, 1.00m, "Drink", "C1"),
+    //        new("Dr. Rumble", 1.50m, 1.25m, "Drink", "C2"),
+    //        new("Mountain Condensate", 1.50m, 1.15m, "Drink", "C3"),
+    //        new("EnergyStroke", 1.50m, 1.00m, "Drink", "C4"),
+
+    //        new("Cinnamon Pastry", 0.85m, 0.70m, "Pastry", "D1"),
+    //        new("Strawberry Pastry", 0.95m, 0.80m, "Pastry", "D2"),
+    //        new("Blueberry Pastry", 0.75m, 0.65m, "Pastry", "D3"),
+    //        new("Apple Pastry", 0.75m, 0.60m, "Pastry", "D4"),
+    //    ];
 }
