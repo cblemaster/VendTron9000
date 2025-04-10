d6672734008de7d08bbd407365a79c2cb1c6a448
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-10 17:05:26 -0400

    add migration

--- src/Modules/Inventory/Persistence/Program.cs
+++ src/Modules/Inventory/Persistence/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Modules.Inventory.Persistence;
+
+const string CONNSTRING = "Server=.;Database=VendTron9000;Trusted_Connection=true;Trust Server Certificate=true";
+
 Console.WriteLine("Welcome to VendTron-9000!!!...");
+
+IHostBuilder builder = Host
+    .CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddDbContext<InventoryDbContext>(options =>
+            options.UseSqlServer(CONNSTRING));
+    });
+IHost host = builder.Build();
