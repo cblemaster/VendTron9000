29d6ebe23b7d73fbb40d9d0f528bd246bde94204
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-09 21:20:27 -0400

    add snack location entity

---
+++ src/Modules/Inventory/Domain/Entities/SnackLocation.cs
@@ -0,0 +1,25 @@
+﻿
+using Modules.Inventory.Domain.ValueObjects;
+
+namespace Modules.Inventory.Domain.Entities;
+
+internal sealed class SnackLocation : Entity<SnackLocation>
+{
+    internal Descriptor Code { get; init; }
+    internal IEnumerable<Snack> Snacks { get; init; } = [];
+    internal override Identifier<SnackLocation> Id { get; init; }
+
+    private SnackLocation() { }
+    
+    public SnackLocation(string code, Guid id)
+    {
+        Code = new Descriptor(code);
+        Id = new Identifier<SnackLocation>(id);
+    }
+
+    public SnackLocation(string code)
+    {
+        Code = new Descriptor(code);
+        Id = new Identifier<SnackLocation>();
+    }
+}
