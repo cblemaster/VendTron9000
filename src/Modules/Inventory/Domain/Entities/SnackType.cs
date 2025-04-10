2728d92903f09ae6ef27fb440c640679ee90871a
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-09 21:21:50 -0400

    update snack type

--- src/Modules/Inventory/Domain/Entities/SnackType.cs
+++ src/Modules/Inventory/Domain/Entities/SnackType.cs
@@ -4,8 +4,8 @@ namespace Modules.Inventory.Domain.Entities;
 internal sealed class SnackType : Entity<SnackType>
 {
     internal string Name { get; init; }
-    internal override Identifier<SnackType> Id { get; init; }
     internal IEnumerable<Snack> Snacks { get; init; } = [];
+    internal override Identifier<SnackType> Id { get; init; }
 
     private SnackType() { }
 
